99f89266f8747f685de563c2ee156f297dc9edd7
Altafalam3 <altaf.alam0032@gmail.com>
2023-05-23 18:10:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Landing from "./screens/Landing";
+import Signup from './components/signup/Signup';
+import Login from './components/login/Login';
+
+import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">Hi</header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
