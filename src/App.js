774c03d2c9263216fdf700493eb1bfc7b2b5751b
Sarahkhan20 <sarahejaz77@gmail.com>
2023-05-24 20:20:48 +0530

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
 import Landing from "./screens/Landing";
-import Signup from './components/signup/Signup';
-import Login from './components/login/Login';
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Landing />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/" element={<Landing />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import logo from '../../assets/logo.svg'
+import footy from '../../assets/Footer.svg'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <img src={logo} alt="" srcset="" />
+      <img src={footy} alt="" srcset="" />
+    </div>
   )
 }
 
