b6428f43a0e3ca5952a0e96c20c2d25eb0441d6b
Sarahkhan20 <sarahejaz77@gmail.com>
2023-06-01 00:27:01 +0530

    quiz

--- src/screens/quiz/Quiz.jsx
+++ src/screens/quiz/Quiz.jsx
@@ -1,61 +1,116 @@
 import { Query } from "appwrite";
 import {  databases } from "../../services/appwriteConfig";
 import React, { useState, useEffect } from "react";
-
+import {  useNavigate } from 'react-router-dom'
 
 
 const QuizCard = () => {
-  const [question, setQuestion] = useState();
-  const [option, setOption] = useState();
-       // const [userDetails, setUserDetails] = useState();
-      
-       //  useEffect( () => {
-       //        const getData = account.get();
-       //        getData.then(
-       //               function (response) {
-       //                      setUserDetails(response)
-       //               },
-       //               function (error) {
-       //                      console.log(error);
-       //               }
-       //        )
-       // }, [userDetails]);
-       //  console.log(userDetails);
+  const [questions, setQuestions] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [answer, setanswer] = useState("")
+  const [id, setId] = useState("")
+ 
+  const navigate = useNavigate();
+   const nextQuestion = () => {
+    if (index < questions.length && options.length - 1) {
+      setIndex(index + 1);
+    } else {
+      console.log("no more questions");
+      navigate('/')
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const promise = databases.updateDocument("6475bc41d08143bd0b2e", "6475bc4f578738f63e3c", id)
 
+    promise.then(
+      function (response) {
+        console.log(response);
+        
+      },
+      function (error) {
+        console.log(error);
+      })
+
+  }
+  
+  console.log(id);
+   
        useEffect(() => {
               const getQuestion = databases.listDocuments("6475bc41d08143bd0b2e", "6475bc4f578738f63e3c",);
               getQuestion.then(
                      function (response) { 
-                            setQuestion(response.documents);   
-                      setOption(response.documents)      
+                            setQuestions(response.documents);   
+                  setOptions(response.documents) 
+                  setId(response)
+                 
+               
                      }, function (error) {
     console.log(error);
 });
        
        }, [])
+ 
+  const currentQuestion = questions[index] || {};
+  const currentOption = options[index] || {};
+  
        
        return (
-          <div>
-      {question && question.map(item => (
-        <div key={item.$id}>
-          <div className="p-4 flex items-center justify-between border-b-2 bg-gray-100 rounded-lg mb-1">
-            <div>
-              <p>{item.question}</p>
-            </div>
-          </div>
-          {option && option.map(item => (
-            <div key={item.$id}>
-              <div className="p-4 flex items-center justify-between border-b-2 bg-gray-100 rounded-lg mb-1">
-                <div>
-                  <p>{item.option2}</p>
-                  <p>{item.option}</p>
-                </div>
-              </div>
-            </div>
-          ))}
+         <div>
+           <form action="" onSubmit={handleSubmit}>
+     {currentQuestion && (
+        <div>
+          <h1>Question:</h1>
+          <p>{currentQuestion.question}</p>
         </div>
-      ))}
-    </div>
+           )}
+           <div>
+        
+           {currentOption.option && (
+          <>
+            <input type="radio" name="option" id="" value={currentOption.option} id="" onChange={(e) => {
+                     setanswer(e.target.value)
+            }} />
+            <label>{currentOption.option}</label>
+          </>
+        )}
+          {currentOption.option2 && (
+          <>
+            <input type="radio" name="option" id="" value={currentOption.option2} id="" onChange={(e) => {
+                     setanswer(e.target.value)
+            }}/>
+            <label>{currentOption.option2}</label>
+          </>
+        )}
+              {currentOption.option3 && (
+          <>
+            <input type="radio" name="option" id="" value={currentOption.option3} id="" onChange={(e) => {
+                     setanswer(e.target.value)
+            }}/>
+            <label>{currentOption.option3}</label>
+          </>
+        )}
+              {currentOption.option4 && (
+          <>
+            <input type="radio" name="option" id="" value={currentOption.option4} id="" onChange={(e) => {
+                     setanswer(e.target.value)
+            }} />
+            <label>{currentOption.option4}</label>
+          </>
+        )}
+             {currentOption.option5 && (
+          <>
+                   <input type="radio" name="option" value={currentOption.option5} id="" onChange={(e) => {
+                     setanswer(e.target.value)
+            }} />
+            <label>{currentOption.option5}</label>
+          </>
+        )}
+      </div>
+             <button onClick={nextQuestion}>Next Question</button>
+             </form>
+  </div>
   );          
 };
 
